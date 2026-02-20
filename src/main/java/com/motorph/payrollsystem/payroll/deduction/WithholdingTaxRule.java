b51ce67a00ce0d4837ca1052e73aec296da24e95
djkier <djjustinekier@gmail.com>
2026-02-20 15:59:23 +0800

    Update tax table

--- src/main/java/com/motorph/payrollsystem/payroll/deduction/WithholdingTaxRule.java
+++ src/main/java/com/motorph/payrollsystem/payroll/deduction/WithholdingTaxRule.java
@@ -16,6 +16,9 @@ public class WithholdingTaxRule extends AbstractDeductionRule {
     *https://sprout.ph/articles/how-to-calculate-withholding-tax-philippines/
     *Date: January 1, 2023
     *TRAIN law
+    *
+    *semi monthly tax table
+    *https://www.payrollsolutions.ph/resources/withholding_tax
     */
     @Override
     public String getName() {
@@ -44,7 +47,23 @@ public class WithholdingTaxRule extends AbstractDeductionRule {
     }
     
     @Override
-    public double computeSemi(double grossPay) {
-        return 0;
+    public double computeSemi(double taxablePay) {
+        double tax;
+        
+        if (taxablePay > 333333) {
+            tax = 91770.70 + ((taxablePay - 333333) * 0.35);
+        } else if (taxablePay > 83333) {
+            tax = 16770.70 + ((taxablePay - 83333) * 0.3);
+        } else if (taxablePay > 33333) {
+            tax = 4270.70 + ((taxablePay - 33333) * 0.25);
+        } else if (taxablePay > 16667) {
+            tax = 937.50 + ((taxablePay - 16667) * 0.20);
+        } else if (taxablePay > 10417) {
+            tax = (taxablePay - 10417) * 0.15;
+        } else {
+            tax = 0;
+        }
+        
+        return tax;
     }
 }
